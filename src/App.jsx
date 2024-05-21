73af95630e963dcff88754197b26a7be9540b192
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-21 17:03:46 +0530

    The Repo is created for this project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+  return (
+    <>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div>
+          3D Models
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
