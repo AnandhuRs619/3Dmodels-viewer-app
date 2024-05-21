1282e51d4af7c2b82b5fe2effcc55608a1d29051
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-21 17:40:30 +0530

    Set Up Firebase db for this app

---
+++ src/db/FirebaseConfig.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBwn4EIKev_SYJ6nRQwtD9PLQhKu2BrO4",
+  authDomain: "d-model-viewer-e5088.firebaseapp.com",
+  projectId: "d-model-viewer-e5088",
+  storageBucket: "d-model-viewer-e5088.appspot.com",
+  messagingSenderId: "972151490441",
+  appId: "1:972151490441:web:c980a97fd2337fd3e20358",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
